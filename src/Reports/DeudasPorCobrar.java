34317bac43e218ed0c010b6bd616ac46b02d4f15
Usuario <Usuario@Manuel>
2024-07-11 23:35:41 -0400

    Antes del PDF

--- src/Reports/DeudasPorCobrar.java
+++ src/Reports/DeudasPorCobrar.java
@@ -45,6 +45,8 @@ public class DeudasPorCobrar extends javax.swing.JInternalFrame {
         MostrarEmpresa(cbxEmpresa);
         AutoCompleteDecorator.decorate(cbxEmpresa);
         MostrarTablaDeuda();
+        txtId.setEnabled(false);
+        txtIdEmpresa.setEnabled(false);
 
     }
 
@@ -872,150 +874,8 @@ double subtotal = 0.0;
         }
         return bandera;
     }
+
     
-//    private void pdf(String tipo) throws FileNotFoundException, IOException {
-//        try {
-//            int id = Integer.parseInt(txtId.getText());
-//            FileOutputStream archivo;
-//            File file = new File("src/pdf/venta" + id + ".pdf");
-//            archivo = new FileOutputStream(file);
-//            Document doc = new Document();
-//            PdfWriter.getInstance((com.itextpdf.text.Document) doc, archivo);
-//            doc.open();
-//            //Insertamos Logo
-//            Image img = Image.getInstance("src/img/logo_pdf.png");
-//            //Nuevo Paragraph
-//            Paragraph fecha = new Paragraph();
-//            Font negrita = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, BaseColor.BLUE);
-//            fecha.add(Chunk.NEWLINE);
-//            Date date = new Date();
-//            fecha.add("Factura:" + id + "\n" + "Fecha: " + new SimpleDateFormat("dd-MM-yyyy").format(date) + "\n\n");
-//            //Tabla Encabezado
-//            PdfPTable Encabezado = new PdfPTable(4);
-//            Encabezado.setWidthPercentage(100);
-//            Encabezado.getDefaultCell().setBorder(0);
-//            float[] ColumnaEncabezado = new float[]{20f, 30f, 70f, 40f};
-//            Encabezado.setWidths(ColumnaEncabezado);
-//            Encabezado.setHorizontalAlignment(Element.ALIGN_LEFT);
-//
-//            Encabezado.addCell(img);
-//
-//            String ruc = txtRucConfig.getText();
-//            String nom = txtNombreConfig.getText();
-//            String tel = txtTelefonoConfig.getText();
-//            String dir = txtDireccionConfig.getText();
-//            String ra = txtRazonConf.getText();
-//
-//            Encabezado.addCell("");
-//            Encabezado.addCell("Ruc: " + ruc + "\nNombre: " + nom + "\nTelefono: " + tel + "\nDireccion: " + dir + "\nRazon: " + ra);
-//            Encabezado.addCell(fecha);
-//            doc.add(Encabezado);
-//            //Cliente
-//            Paragraph cli = new Paragraph();
-//            cli.add(Chunk.NEWLINE);
-//            cli.add("Datos de los Clientes" + "\n\n");
-//            doc.add(cli);
-//            //Tabla para los datos del cliente
-//            PdfPTable tablacli = new PdfPTable(4);
-//            tablacli.setWidthPercentage(100);
-//            tablacli.getDefaultCell().setBorder(0);
-//            float[] Columnacli = new float[]{20f, 50f, 30f, 40f};
-//            tablacli.setWidths(Columnacli);
-//            tablacli.setHorizontalAlignment(Element.ALIGN_LEFT);
-//            //Agregamos los titulos
-//            PdfPCell cl1 = new PdfPCell(new Phrase("Dni/Ruc", negrita));
-//            PdfPCell cl2 = new PdfPCell(new Phrase("Nombre", negrita));
-//            PdfPCell cl3 = new PdfPCell(new Phrase("Telefono", negrita));
-//            PdfPCell cl4 = new PdfPCell(new Phrase("Direccion", negrita));
-//            cl1.setBorder(0);
-//            cl2.setBorder(0);
-//            cl3.setBorder(0);
-//            cl4.setBorder(0);
-//            //Agregamos las celdas a la Tabla
-//            tablacli.addCell(cl1);
-//            tablacli.addCell(cl2);
-//            tablacli.addCell(cl3);
-//            tablacli.addCell(cl4);
-//            tablacli.addCell(txtRucVenta.getText());
-//            tablacli.addCell(txtCliente.getText());
-//            tablacli.addCell(txtTelefonoCliente.getText());
-//            tablacli.addCell(txtDireccion.getText());
-//            //Agregamos al Documento
-//            doc.add(tablacli);
-//
-////Poductos
-//            PdfPTable tablapro = new PdfPTable(4);
-//            tablapro.setWidthPercentage(100);
-//            tablapro.getDefaultCell().setBorder(0);
-//            float[] Columnapro = new float[]{10f, 50f, 15f, 20f};
-//            tablapro.setWidths(Columnapro);
-//            tablapro.setHorizontalAlignment(Element.ALIGN_LEFT);
-//            PdfPCell pro1 = new PdfPCell(new Phrase("Cant.", negrita));
-//            PdfPCell pro2 = new PdfPCell(new Phrase("Descripcion", negrita));
-//            PdfPCell pro3 = new PdfPCell(new Phrase("Precio U.", negrita));
-//            PdfPCell pro4 = new PdfPCell(new Phrase("Precio T.", negrita));
-//            pro1.setBorder(0);
-//            pro2.setBorder(0);
-//            pro3.setBorder(0);
-//            pro4.setBorder(0);
-//            pro1.setBackgroundColor(BaseColor.DARK_GRAY);
-//            pro2.setBackgroundColor(BaseColor.DARK_GRAY);
-//            pro3.setBackgroundColor(BaseColor.DARK_GRAY);
-//            pro4.setBackgroundColor(BaseColor.DARK_GRAY);
-//            tablapro.addCell(pro1);
-//            tablapro.addCell(pro2);
-//            tablapro.addCell(pro3);
-//            tablapro.addCell(pro4);
-//            
-//            if (tipo.equals("individual")) {
-//                // Generar PDF para un cliente individual
-//                tablapro.addCell("1");
-//                tablapro.addCell(txtCliente.getText());
-//                tablapro.addCell(txtST.getText());
-//                tablapro.addCell(txtTotal.getText());
-//            } else {
-//                // Generar PDF para un grupo de clientes seleccionados
-//                for (int i = 0; i < tbDeudasPorCobrar.getRowCount(); i++) {
-//                    boolean isSelected = (boolean) tbDeudasPorCobrar.getValueAt(i, 0);
-//                    if (isSelected) {
-//                        String producto = tbDeudasPorCobrar.getValueAt(i, 2).toString();
-//                        String cantidad = "1";
-//                        String precio = tbDeudasPorCobrar.getValueAt(i, 12).toString();
-//                        String total = tbDeudasPorCobrar.getValueAt(i, 12).toString();
-//                        tablapro.addCell(cantidad);
-//                        tablapro.addCell(producto);
-//                        tablapro.addCell(precio);
-//                        tablapro.addCell(total);
-//                    }
-//                }
-//            }
-//            
-//            doc.add(tablapro);
-//            //Nuevo Paragraph para el total
-//            Paragraph info = new Paragraph();
-//            info.add(Chunk.NEWLINE);
-//            info.add("Total a Pagar: " + txtTotal.getText());
-//            info.setAlignment(Element.ALIGN_RIGHT);
-//            doc.add(info);
-//            //Paragraph para la firma
-//            Paragraph firma = new Paragraph();
-//            firma.add(Chunk.NEWLINE);
-//            firma.add("Cancelacion y Firma\n\n");
-//            firma.add("------------------------");
-//            firma.setAlignment(Element.ALIGN_CENTER);
-//            doc.add(firma);
-//            //Paragraph para el Mensaje
-//            Paragraph mensaje = new Paragraph();
-//            mensaje.add(Chunk.NEWLINE);
-//            mensaje.add("Gracias por su Preferencia");
-//            mensaje.setAlignment(Element.ALIGN_CENTER);
-//            doc.add(mensaje);
-//
-//            doc.close();
-//            archivo.close();
-//            Desktop.getDesktop().open(file);
-//        } catch (DocumentException e) {
-//            System.out.println(e.toString());
-//        }
-//    }
+
+    
 }
